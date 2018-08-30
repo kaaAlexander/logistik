dba0de07e9fd911d3c751d66c166b6f10070ec1a
kaaAlexander <cops2002@mail.ru>
2018-08-30 18:02:48 +0300

    entity complete

---
+++ src/main/java/by/tut/alexander/kaa/logistik/user/controller/UserController.java
@@ -0,0 +1,10 @@
+package by.tut.alexander.kaa.logistik.user.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by GM on 30.08.2018.
+ */
+@RestController
+public class UserController {
+}
